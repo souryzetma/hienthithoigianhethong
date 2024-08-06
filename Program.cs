f6fd239a151fdbac7436b21f726f94057a56c104
unknown <souryzetma1144@yahoo.com>
2024-08-06 13:40:59 +0700

    comit 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnet_system_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+    }
+}
